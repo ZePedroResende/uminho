0bd2ea7f79f0c401ff4dd83dad7ca8b804a75471
NEO <neo@localhost.localdomain>
2016-02-26 10:19:17 +0000

    perguntas 5-7

---
+++ Programaçao/50perguntas/5.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+// numero de 1 em binario de um certo numero
+int bitsUm (unsigned int n)
+{
+	int cont = 0;
+	int rest;
+
+	while(n != 0)
+	{
+		rest = n % 2;
+		n = n / 2;
+		cont = cont + rest;
+	}
+	return cont;
+}
+
+
+// numero de zeros em binario na representaçao final
+int trailingZ (unsigned int n) {
+
+    int r ;
+    r = n %2 ;
+    n = n / 2;
+    int i = 0;
+
+
+    while (r != 1 ){
+    r =n % 2 ;
+     n = n /2 ;
+     i++;
+    }
+
+    return i;
+}
+//
+int qDig (int n) {
+    int r = 0;
+
+    while (n > 0){
+    n =  n /10;
+    r++;
+    }
+
+    return r;
+}
+//
+int strlen1 (char s1[], char s2[]){
+
+
+}
+
+//
+int main() {
+int a;
+a = trailingZ(6);
+printf("%d\n",a);
+  return 0;
+}
--- li/html/search/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['estado_5finicial',['ESTADO_INICIAL',['../cartas_8c.html#ae498e2935abe10d697a8b6323f47728b',1,'cartas.c']]]
-];
