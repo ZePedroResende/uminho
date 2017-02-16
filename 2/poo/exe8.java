2c193957081ea7543b9fa6f2f61d037721617e0a
ZePedroResende <j.p.resende68@gmail.com>
2017-02-16 16:14:51 +0000

    ficha cp

--- 2/poo/exe8.java
+++ 2/poo/exe8.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 class exe8{
+    
     public static void main(String[] argv){
         Scanner diaScanner = new Scanner(System.in);
         int dia = diaScanner.nextInt();
@@ -7,17 +8,15 @@ class exe8{
         int mes = diaScanner.nextInt();
         Scanner anoScanner = new Scanner(System.in);
         int ano = diaScanner.nextInt();
-
-       int resultado = (ano -1900) * 365;
-       resultado += (ano-1900)/4;
-       if( bissexto(ano) && (mes == 1 || mes == 2)){
+        int resultado = (ano -1900) * 365;
+        resultado += (ano-1900)/4;
+        if( bissexto(ano) && (mes == 1 || mes == 2)){
             ano -- ;
-       }
+        }
         ano = ano%7;
         System.out.println(ano);
     }
-
-
+    
     public static boolean bissexto(int year){
     return ((year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0));
     }
