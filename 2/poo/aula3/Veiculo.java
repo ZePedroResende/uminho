60a4bdb6ae2643a07a97680221c7fc23cb7b984e
ZePedroResende <j.p.resende68@gmail.com>
2017-03-02 17:48:03 +0000

    aula so

--- 2/poo/aula3/Veiculo.java
+++ 2/poo/aula3/Veiculo.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * Write a description of class Veiculo here.
  * 
@@ -15,38 +15,80 @@ public class Veiculo
     private int capacidade; 
     private int conteudo;
 
-    public void abastecer(int litros)
-    {
 
+    public String getmatricula(){
+        return matricula;
+    }
+    public void setmatricula(String matricula){
+        this.matricula = matricula;
     }
 
-    public void resetKms()
-    {
 
+    public double getKmsTotal(){
+        return kmsTotal;
+    }
+    public void setKmsTotal( double kmsTotal){
+        this.kmsTotal = kmsTotal;
     }
 
-    public double autonomia()
-    {
-        return ;
+    public double getKmsParcial(){
+        return kmsParcial;
+    }
+    public void setKmsParcial(double kmsParcial){
+        this.kmsParcial = kmsParcial;
     }
 
-    public void registarViagem(int kms, double consumo)
-    {
-
+    public double getConsumoMedio(){
+        return consumoMedio;
+    }
+    public void setConsumoMedio(double consumoMedio){
+        this.consumoMedio = consumoMedio;
     }
 
-    public boolean naReserva()
-    {
-
+    public int getCapacidade(){
+        return capacidade;
+    }
+    public void setCapacidade(int capacidade){
+        this.capacidade = capacidade;
     }
 
-    public double totalCombustivel(double custoLitro)
-    {
-
+    public int getConteudo(){
+        return conteudo;
+    }
+    public void setConteudo(int conteudo){
+        this.conteudo = conteudo;
     }
 
-    public double custoMedioKm(double custoLitro)
-    {
+    public void abastecer(int litros){
+        this.conteudo = this.conteudo + litros;
+        if(this.conteudo > this.capacidade) this.conteudo = this.capacidade;
+    }
 
+    public void resetKms(){
+        this.kmsParcial = 0;
+        this.consumoMedio = 0;
+    }
+
+    public double autonomia(){
+        return (this.conteudo/this.consumoMedio)*100;
+    }
+
+    public void registarViagens(int kms, double consumo){
+        this.consumoMedio = consumo*100/kms;
+        this.kmsTotal += kms;
+        this.kmsParcial += kms;
+        this.conteudo-=consumo;
+    }
+
+    public boolean naReserva(){
+        return (this.conteudo <= 10);
+    }
+
+    public double totalCombustivel(double custoLitro){
+        return this.conteudo*custoLitro;
+    }
+
+    public double custoMedioKms(double custoLitro){
+        return (this.consumoMedio*custoLitro)/100;
     }
 }
