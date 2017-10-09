6d14e18037ddca52202d79683364f0289bdd69ed
ZePedroResende <j.p.resende68@gmail.com>
2017-10-09 01:00:11 +0100

    SD

---
+++ 3/SD/guiao1/src/com/company/ex3.java
@@ -0,0 +1,58 @@
+package com.company;
+
+class Counter {
+    int num;
+
+    Counter() {
+        num = 0;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void increment() {
+        num += 1;
+    }
+
+    public String toString() {
+        return Integer.toString(num);
+    }
+}
+
+class Runnable3 implements Runnable{
+    Counter C;
+
+    public Runnable3() {
+        C = new Counter();
+    }
+
+    public void run(){
+        C.num++;
+    }
+
+}
+
+public class Main  {
+
+    public static void main(String[] args) throws InterruptedException {
+        int N = 10 ;
+        int i = 5;
+
+        Thread[] ThreadArray = new Thread[N];
+        Runnable2 r2 = new Runnable2();
+        for(int a = 0 ; a < N; a++){
+            ThreadArray[a] = new Thread(r2);
+        }
+
+        for(int a = 0 ; a < N; a++){
+            ThreadArray[a].start();
+        }
+        for(int a = 0 ; a < N; a++){
+            ThreadArray[a].join();
+        }
+
+        System.out.print(r2.C.getNum());
+    }
+}
+
---
+++ 3/SD/untitled/src/N.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class N {
+    private JButton button1;
+    private JPanel panel1;
+
+    public N() {
+        button1.addActionListener(new ActionListener() {
+            System.out.printl("")
+        });
+    }
+}
